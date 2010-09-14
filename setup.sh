947f01aaba2ce482555add39afee44a0e0085e80
James Peach <jorgar@gmail.com>
2010-09-13 20:54:09 -0700

    Add simple setup script.

---
+++ setup.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+# Copyright 2010 James Peach
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+error()
+{
+    echo "$@"
+    exit 1
+}
+
+HOMEDIR=$(cd ~ && pwd)
+VIMRC=$(cd $(dirname $0) && pwd)
+
+# Trim a leading $HOMEDIR from the VIMRC path, which turns it into a relative
+# path if it's inside $HOMEDIR. If it's an absolute path, we will lose the
+# leading '/', but put it back later.
+VIMRC=${VIMRC##$HOMEDIR/}
+
+(
+    cd ~
+
+    [[ -d "$VIMRC" ]] || VIMRC="/${VIMRC}"
+    [[ -d "$VIMRC" ]] || error "can't find $VIMRC"
+
+    echo "linking ~/.vimrc -> $VIMRC/vimrc"
+    ln -sf $VIMRC/vimrc .vimrc
+
+    echo "linking ~/.vim -> $VIMRC"
+    ln -sf $VIMRC .vim
+)
