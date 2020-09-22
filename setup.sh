4a59d351838da33dc6750bfaa24f2ee8cbb0d675
James Peach <jpeach@apache.org>
2020-09-22 10:39:44 +1000

    Add git pull config.

--- setup.sh
+++ setup.sh
@@ -200,3 +200,4 @@ git::config user.email      jpeach@apache.org
 git::config diff.noprefix   true
 git::config rerere.enabled  true # https://git-scm.com/book/en/v2/Git-Tools-Rerere
 git::config grep.extendedRegexp true
+git::config pull.ff only
